17fbcc45f4fb25eac6f44a596ac60f97ca9c5519
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-16 18:03:46 -0700

    many tests passing

--- src/car.js
+++ src/car.js
@@ -1,16 +1,59 @@
+// require('locus');
+
 function Car(make, model, year, color){
   this.make = make;
-  this.model = model
-  // do the same for year and color
-  this.state = "off"
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.state = "off";
   this.previousOwners = [];
   this.currentOwner = "manufacturer";
 }
 
+// Car#sale, we should be able to sale a car to someone, which should update the current_owner and previous_owners array.
 Car.prototype.sale = function(newOwner){
-
+  this.previousOwners.push(this.currentOwner); 
+  this.currentOwner = newOwner;
 };
 
+// Car#paint, which should take a new color and update the color of the car to be new color.
 Car.prototype.paint = function(newColor){
+  this.color = newColor || black;
+};
+
+// // Car#start should change the state of the car to on.
+Car.prototype.start = function(){
+  this.state = "on";
+};
+
+// // Car#off should change the state to off.
+Car.prototype.off = function(){
+  this.state = "off";
+};
+
+// // Car#driveTo, it should console.log "driving to <destination>", but only if the car state is on. Note: this method needs to take a destination param.
+Car.prototype.driveTo = function(destination){
+  //need to start car, test
+  if(this.state === "on"){  
+    console.log("driving to " + destination);
+  }
+  // return destination;
 
 };
+
+// // Car#park only if the car is off and if it's off console.log parked!!
+// Car.prototype.park = function(){
+
+// };
+
+
+
+var frederick = new Car("vw", "bug", 1965, "green");
+
+var kitt = new Car("hotrod", "awesome", 1985, "black");
+
+
+module.exports = frederick;
+
+
+// eval(locus);
---
+++ test/carTest.js
@@ -0,0 +1,80 @@
+var frederick = require("../src/car.js"),
+  expect = require("chai").expect;
+
+describe("frederick the car/prototype", function(){
+
+  describe("is frederick being created", function(){
+    it("should exist", function(){
+      expect(frederick).to.exist;
+    })
+  })
+
+  describe("color", function(){
+    it("the color of the car should exist", function(){
+      expect(frederick.color).to.exist;
+    });
+  });
+
+  describe("color", function(){
+    it("should be green", function(){
+      expect(frederick.color).to.equal("green");
+    });
+  });
+
+  describe("state property", function(){
+    it("should be off for a new car", function(){
+      expect(frederick.state).to.equal("off");
+    });
+  });
+
+  describe("#paint", function(){
+    it("should change to yellow", function(){
+      frederick.paint("yellow");
+      //frederick.paint("green");
+      expect(frederick.color).to.equal("yellow");
+    })
+  })
+
+  describe("#sale", function(){
+    it("should update the owner", function(){
+      frederick.sale("Lisa");
+      expect(frederick.currentOwner).to.equal("Lisa");
+    });
+  });
+
+  describe("#start", function(){
+    it("should be on", function(){
+      frederick.start();
+      expect(frederick.state).to.equal("on");
+    })
+  })
+
+  describe("#off", function(){
+    it("should be off", function(){
+      frederick.off();
+      expect(frederick.state).to.equal("off");
+    })
+  })
+
+  describe("#driveTo", function(){
+    it("should console.log the destination, return it for testing", function(){
+      var spot = frederick.driveTo("Mexico");
+      expect(spot).to.exist;
+    })
+  })
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
